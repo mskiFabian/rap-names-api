ee14b6cca282a5b6ad3f2cf5a7cdad28095b0d0c
Fabian Radomski <cosos123@o2.pl>
2023-05-29 18:22:22 +0200

    working with rapers api

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const rappers = {
+    '21 savage': {
+        'age': 30,
+        'birthName': 'Shéyaa Bin Abraham-Joseph',
+        'birthLocation': 'London, England'
+    },
+    'chance the rapper': {
+        'age': 29,
+        'birthName': 'Chancelor Bennett',
+        'birthLocation': 'Chicago, USA'
+    },
+    'unknown': {
+        'age': 0,
+        'birthName': 'unknown',
+        'birthLocation': 'unknown'   
+    }
+}
+
+
+app.get('/', (req,res) => {
+    // where to look for index.html
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:name', (req,res) => {
+    const rapperName = req.params.name.toLowerCase()
+    if(rappers[rapperName]) {
+        res.json(rappers[rapperName])
+    } else {
+        res.json(rappers['unknown'])
+    }
+    
+})
+
+app.listen(PORT, () => {
+    console.log(`The server is now running on port ${PORT}`)
+})
